c28ad0df049f57a580b168b48c4ba06cbd678623
pharia-le <pharia.le@gmail.com>
2020-09-02 13:03:32 -0700

    Done.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reverseStr = ''
+    for (let i=str.length-1; i>=0; i--) {
+        reverseStr+=str[i]
+    }
+    return reverseStr
+}
 
 module.exports = reverse;
