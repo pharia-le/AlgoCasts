485a0f561704aaf771afe2c08fd8e3970da48177
pharia-le <pharia.le@gmail.com>
2020-09-02 16:43:15 -0700

    Done.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let neg = false
+    if (n < 0) {
+        neg = true
+        n*=-1
+    }
+    return n.toString().split('').reverse().join('')*(neg ? -1 : 1)
+}
 
 module.exports = reverseInt;
