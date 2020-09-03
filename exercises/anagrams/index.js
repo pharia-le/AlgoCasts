d9a1aab8319e02bf9435c10994454eab41afb023
pharia-le <pharia.le@gmail.com>
2020-09-03 02:12:02 -0700

    Done.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,28 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    function charMap(str) {
+        const mod = str.replace(/[^\w]/g,'').toLowerCase()
+        let charMap = {}
+        mod.split('').reduce((chars, char) => {
+            charMap[char] = charMap + 1 || 1;
+            return chars
+        }, {})
+        return charMap
+    }
+    let charMapA = charMap(stringA)
+    let charMapB = charMap(stringB)
+    let longerMap = charMapB
+    let shorterMap = charMapA
+    if (Object.keys(charMapA).length >= Object.keys(charMapB).length) {
+        longerMap = charMapA
+        shorterMap = charMapB
+    }
+    for (let char in longerMap) {
+        if (!shorterMap[char] || shorterMap[char] !== longerMap[char]) return false
+    }
+    return true
+}
 
 module.exports = anagrams;
