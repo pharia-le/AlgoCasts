c6166d967734eb8f0d8b08db047bb6c232f79608
pharia-le <pharia.le@gmail.com>
2020-09-02 23:27:42 -0700

    Add solution w/ logical OR operator.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,16 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+    // const strCount = str.split('').reduce((count, char) => {
+    //     if (!count[char]) {
+    //         count[char] = 1
+    //     } else {
+    //         count[char] = count[char] + 1
+    //     }
+    //     return count
+    // }, {})
+//     return Object.keys(strCount).sort((a,b) => strCount[b] - strCount[a])[0]
+// }
+
 function maxChar(str) {
-    const strCount = str.split('').reduce((count, char) => {
-        if (!count[char]) {
-            count[char] = 1
-        } else {
-            count[char] = count[char] + 1
-        }
-        return count
-    }, {})
-    return Object.keys(strCount).sort((a,b) => strCount[b] - strCount[a])[0]
+    const charMap = str.split('')
+        .reduce((chars, char) => {
+            chars[char] = chars[char] + 1 || 1
+            return chars
+        },{})
+    return Object.keys(charMap).sort((a,b) => charMap[b] - charMap[a])[0]
 }
 
 module.exports = maxChar;
