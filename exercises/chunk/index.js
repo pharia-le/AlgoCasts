5f6c12329cd9ebd35379bf5b8411cf9d273d0013
pharia-le <pharia.le@gmail.com>
2020-09-03 01:33:17 -0700

    Add additional solutions.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,15 +8,36 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// function chunk(array, size) {
+//     let arrays = []
+    // for (let i=size; i<=array.length; i+=size) {
+    //     arrays.push(array.slice(i-size, i))
+    // }
+//     if (array.length % size !== 0) {
+//         arrays.push(array.slice(array.length - (array.length % size)))
+//     }
+//     return arrays
+// }
+
+// function chunk(array, size) {
+//     let chunked = []
+//     for (let ele of array) {
+//         const last = chunked[chunked.length-1]
+//         if (!last || last.length === size) {
+//             chunked.push([ele])
+//         } else {
+//             last.push(ele)
+//         }
+//     }
+//     return chunked
+// }
+
 function chunk(array, size) {
-    let arrays = []
-    for (let i=size; i<=array.length; i+=size) {
-        arrays.push(array.slice(i-size, i))
+    let chunked = []
+    for (let i=0; i<array.length; i+=size) {
+        chunked.push(array.slice(i, i + size))
     }
-    if (array.length % size !== 0) {
-        arrays.push(array.slice(array.length - (array.length % size)))
-    }
-    return arrays
+    return chunked
 }
 
 module.exports = chunk;
