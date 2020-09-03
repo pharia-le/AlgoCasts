a2949bc9aa62ce02011affc9be2c4679b63f6cf8
pharia-le <pharia.le@gmail.com>
2020-09-03 11:37:49 -0700

    Add solution w/ sort().

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,28 +8,63 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+//     function charMap(str) {
+        // const mod = str.replace(/[^\w]/g,'').toLowerCase()
+        // let charMap = {}
+        // mod.split('').reduce((chars, char) => {
+        //     charMap[char] = charMap + 1 || 1;
+        //     return chars
+        // }, {})
+        // return charMap
+//     }
+    // let charMapA = charMap(stringA)
+    // let charMapB = charMap(stringB)
+//     let longerMap = charMapB
+//     let shorterMap = charMapA
+//     if (Object.keys(charMapA).length >= Object.keys(charMapB).length) {
+//         longerMap = charMapA
+//         shorterMap = charMapB
+//     }
+//     for (let char in longerMap) {
+//         if (!shorterMap[char] || shorterMap[char] !== longerMap[char]) return false
+//     }
+//     return true
+// }
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA)
+//     const bCharMap = buildCharMap(stringB)
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false
+//     }
+
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) return false
+//     }
+
+//     return true
+// }
+
+// function buildCharMap(str) {
+//     let charMap = {}
+
+//     str.replace(/[^\w]/g,'').toLowerCase().split('')
+//         .reduce((chars, char) => {
+//             charMap[char] = charMap + 1 || 1;
+//             return chars
+//         }, {})
+        
+//     return charMap
+// }
+
 function anagrams(stringA, stringB) {
-    function charMap(str) {
-        const mod = str.replace(/[^\w]/g,'').toLowerCase()
-        let charMap = {}
-        mod.split('').reduce((chars, char) => {
-            charMap[char] = charMap + 1 || 1;
-            return chars
-        }, {})
-        return charMap
-    }
-    let charMapA = charMap(stringA)
-    let charMapB = charMap(stringB)
-    let longerMap = charMapB
-    let shorterMap = charMapA
-    if (Object.keys(charMapA).length >= Object.keys(charMapB).length) {
-        longerMap = charMapA
-        shorterMap = charMapB
-    }
-    for (let char in longerMap) {
-        if (!shorterMap[char] || shorterMap[char] !== longerMap[char]) return false
-    }
-    return true
+    return cleanStr(stringA) === cleanStr(stringB)
+}
+
+function cleanStr(str) {
+    return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
 }
 
 module.exports = anagrams;
