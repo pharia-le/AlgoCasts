96d4d764bc325ccd78d16807d00cb5da8b05c5a8
pharia-le <pharia.le@gmail.com>
2020-09-11 19:01:35 -0700

    Add secondary solution.

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,16 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    let seq = [ 0 ]
-    while (seq.length <= n) {
-        if (seq.length === 1) {
-            seq[1] = 1
-        } else {
-            seq[seq.length] = seq[seq.length-1] + seq[seq.length-2]
-        }
+// function fib(n) {
+//     let seq = [ 0 ]
+//     while (seq.length <= n) {
+//         if (seq.length === 1) {
+//             seq[1] = 1
+//         } else {
+//             seq[seq.length] = seq[seq.length-1] + seq[seq.length-2]
+//         }
+//     }
+//     return seq[seq.length-1]
+// }
+
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      if (cache[args]) {
+        return cache[args];
+      }
+  
+      const result = fn.apply(this, args);
+      cache[args] = result;
+  
+      return result;
+    };
+  }
+  
+  function slowFib(n) {
+    if (n < 2) {
+      return n;
     }
-    return seq[seq.length-1]
-}
+  
+    return fib(n - 1) + fib(n - 2);
+  }
+  
+  const fib = memoize(slowFib);
 
 module.exports = fib;
