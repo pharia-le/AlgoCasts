913a5b9564c57636026c3056b7aa24ddf27cdd26
pharia-le <pharia.le@gmail.com>
2020-12-14 02:35:02 -0800

    Add multiple solutions.

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -12,37 +12,68 @@
 // and return the Node in the tree with the same value.
 
 class Node {
-    constructor(data) {
-      this.data = data;
-      this.left = null;
-      this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  // insert(data) {
+  //   if (data < this.data) {
+  //     if (!this.left) {
+  //       this.left = new Node(data)
+  //       return
+  //     }
+  //     this.left.insert(data)
+  //   }
+  //   if (data > this.data) {
+  //     if (!this.right) {
+  //       this.right = new Node(data)
+  //       return
+  //     }
+  //     this.right.insert(data)
+  //   }
+  // }
   
-    insert(data) {
-      if (data < this.data && this.left) {
-        this.left.insert(data);
-      } else if (data < this.data) {
-        this.left = new Node(data);
-      } else if (data > this.data && this.right) {
-        this.right.insert(data);
-      } else if (data > this.data) {
-        this.right = new Node(data);
-      }
-    }
-  
-    contains(data) {
-      if (this.data === data) {
-        return this;
-      }
-  
-      if (this.data < data && this.right) {
-        return this.right.contains(data);
-      } else if (this.data > data && this.left) {
-        return this.left.contains(data);
-      }
-  
-      return null;
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
     }
   }
+
+  // contains(data) {
+  //   if (data === this.data) {
+  //     return this;
+  //   } else if (data < this.data && !this.left) {
+  //     return null;
+  //   } else if (data < this.data) {
+  //     return this.left.contains(data);
+  //   } else if (data > this.data && !this.right) {
+  //     return null;
+  //   } else if (data > this.data) {
+  //     return this.right.contains(data);
+  //   }
+  // }
+
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    return null;
+  }
+}
   
 module.exports = Node;
