b182a9b05c152fcd44a9f06399994de2fd061af6
pharia-le <pharia.le@gmail.com>
2020-12-14 02:01:38 -0800

    Update solution.

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -12,19 +12,22 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
-    const arr = [root, 's'];
-    const counters = [0];
-  
-    while (arr.length > 1) {
-      const node = arr.shift();
-  
-      if (node === 's') {
-        counters.push(0);
-        arr.push('s');
-      } else {
-        arr.push(...node.children);
-        counters[counters.length - 1]++;
-      }
+ const counters = [ 0 ];
+ const arr = [ root, null ];
+
+ while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === null) {
+      arr.push(node);
+      counters.push(0)
+    } else {
+      arr.push(...node.children);
+      counters[counters.length-1]++;
     }
+ }
+
+ return counters
+}
 
 module.exports = levelWidth;
