4ac3847c451f5e08b0956be46849f59e581f7489
pharia-le <pharia.le@gmail.com>
2020-12-17 23:15:40 -0800

    Remove commented code.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,18 +5,6 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// function maxChar(str) {
-    // const strCount = str.split('').reduce((count, char) => {
-    //     if (!count[char]) {
-    //         count[char] = 1
-    //     } else {
-    //         count[char] = count[char] + 1
-    //     }
-    //     return count
-    // }, {})
-//     return Object.keys(strCount).sort((a,b) => strCount[b] - strCount[a])[0]
-// }
-
 function maxChar(str) {
     const charMap = str.split('')
         .reduce((chars, char) => {
